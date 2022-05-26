b059fcc730872699d21e4546de27c674daa2974a
ImranSyafiq <104118579+ImranSyafiq@users.noreply.github.com>
2022-05-26 09:51:11 +0800

    Add files via upload

---
+++ practical.php
@@ -0,0 +1,106 @@
+<?php
+    $fname=$_POST["fname"];
+    $lname=$_POST["lname"];
+    $age=$_POST["age"];
+    $state=$_POST["state"];
+
+    /*
+    Connect to DB
+
+    $servername="localhost";
+    $username="root";
+    $password="";
+
+    $conn=new mysqli($servername,$username,$password);
+
+    if($conn->connect_error){
+        die("Connection Unsuccessful:".$conn->connect_error);
+    }else{
+        echo "Connection successful<br><br>";
+    }
+
+    $conn->close();
+    */
+
+    /*
+    Create Database
+
+    $servername="localhost";
+    $username="root";
+    $password="";
+
+    $conn=new mysqli($servername,$username,$password);
+
+    if($conn->connect_error){
+        die("Connection Unsuccessful:".$conn->connect_error);
+    }else{
+        echo "Connection successful<br><br>";
+    }
+
+    $sql="CREATE DATABASE db_register";
+
+    if($conn->query($sql)===TRUE){
+        echo "Database Successfully Created<br>";
+    }else{
+        echo "Databse Unsuccessfully Created:".$conn->error;
+    }
+
+    $conn->close();
+    */
+
+    /*
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname="db_register";
+
+    $conn=new mysqli($servername,$username,$password,$dbname);
+
+    if($conn->connect_error){
+        die("Connection Unsuccessful:".$conn->connect_error);
+    }else{
+        echo "Connection successful<br><br>";
+    }
+
+    $sql="CREATE TABLE user_registration (FirstName VARCHAR(30) NOT NULL,LastName VARCHAR(30) NOT NULL,Age INT(3) NOT NULL,State VARCHAR(30) NOT NULL)";
+
+    if($conn->query($sql)===TRUE){
+        echo "Table Successfully Created<br>";
+    }else{
+        echo "Table Unsuccessfully Created:".$conn->error;
+    }
+
+    $conn->close();
+    */
+
+    /* */
+
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname="db_register";
+
+    $conn=new mysqli($servername,$username,$password,$dbname);
+
+    if($conn->connect_error){
+        die("Connection Unsuccessful:".$conn->connect_error);
+    }else{
+        echo "Connection successful<br><br>";
+    }
+
+    $sql="INSERT INTO user_registration (FirstName,LastName,Age,State) VALUES ('$fname','$lname','$age','$state')";
+
+    if($conn->query($sql)===TRUE){
+        echo "Data Insert Successfully<br>";
+    }else{
+        echo "Data Insert Unsuccessfully:".$conn->error;
+    }
+
+    $conn->close();
+
+    echo "Welcome ".$fname." !<br>";
+    echo "Your name is ".$fname." ".$lname."<br>";
+    echo "You are ".$age." years old and staying at ".$state.".";
+
+
+?>
